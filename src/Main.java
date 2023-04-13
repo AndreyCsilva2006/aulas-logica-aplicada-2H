2fc47510e62012d19400aceaade052e3b4fafd50
AndreyCsilva2006 <csilvaandrey2006@gmail.com>
2023-04-13 08:16:56 -0300

    Segundo Commit
    Adicionado
    -Laço de Repetição com while e for.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int cont = 0;
+        // laço de repetição
+        while(cont <= 10) {
+            System.out.println("Contador = "+cont);
+            // incrementação
+            cont++;
+        }
+
+        // laço para caça
+        for(int x = 0; x <= 10; x++) {
+            System.out.println("Contador 2 = "+x);
+        }
     }
 }
