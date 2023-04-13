105207869eda2b5d8bff57945efd201029c07b57
AndreyCsilva2006 <csilvaandrey2006@gmail.com>
2023-04-13 08:25:58 -0300

    Terceiro Commit
    Adicionado
    -Laço de Repetição com do while.

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,13 @@ public class Main {
         for(int x = 0; x <= 10; x++) {
             System.out.println("Contador 2 = "+x);
         }
+        // código faça enquanto
+        int cont2 = 1;
+        do{
+            // processo
+            System.out.println(cont2);
+            // incremento
+            cont2++;
+        }while(cont2 <= 10);
     }
 }
