7652e6019b4cee3c24fa3f70ca05efea6eebdf08
AndreyCsilva2006 <csilvaandrey2006@gmail.com>
2023-05-18 11:05:16 -0300

    aula de OO em java.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // Aula introdutoria de Orientação e Objetos
-
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
     }
 }
